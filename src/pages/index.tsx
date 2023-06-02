463324b5e0500c4bbfab2bdd44024369f0f3d6e3
Emil <you@example.com>
2023-06-02 13:23:34 +0400

    Set up signup fetch to backend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,10 @@ import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 import { AppButton } from '../components/AppButton/AppButton'
 import NoteCard from '@/components/NoteCard/NoteCard'
+import { useRouter } from 'next/router'
 
 export default function Home() {
+  const router = useRouter()
   return (
     <div className={styles.container}>
       <div className={styles.seperator}>
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,12 +1,33 @@
 import styles from '../styles/Signup.module.css'
+import { useRouter } from 'next/router';
+
 
 export default function signup() {
+  const router = useRouter();
+
+  const submit = async (event: any) => {
+    event.preventDefault();
+    const body = { email: event.target.email.value, password: event.target.password.value }
+
+    const data = await fetch(`http://localhost:3001/users/signup`, {
+      method: 'Post',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+      mode: 'cors'
+    });
+    const { access_token } = await data.json();
+    document.cookie = "access_token=" + access_token;
+    router.push('/')
+  }
+
   return (
-    <form action={process.env.BACKEND_URL + '/users/new'} className={styles.form} method='Post'>
+    <form onSubmit={(e) => submit(e)} className={styles.form} method='Post'>
       <label htmlFor="email" className={styles.label}>Your Email</label>
-      <input type="email" name='email' className={styles.input}/>
+      <input type="email" name='email' className={styles.input} required/>
       <label htmlFor="password" className={styles.label}>Enter Password</label>
-      <input type="password" name='password' className={styles.input} />
+      <input type="password" name='password' className={styles.input} required/>
       <button type='submit' className={styles.button}>Create Account</button>
     </form>
   )
