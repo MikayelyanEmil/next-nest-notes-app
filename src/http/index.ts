53d4b9d3a8f1fb0610770ed6d5c0b3831993d4e7
MikayelyanEmil <129506233+MikayelyanEmil@users.noreply.github.com>
2023-08-10 14:20:01 +0400

    Ready to deploy

--- src/http/index.ts
+++ src/http/index.ts
@@ -16,7 +16,7 @@ api.interceptors.response.use(config => config, async (error) => {
     const except = error.config.url != 'auth/login' && error.config.url != 'auth/signup';
     if (error.response?.status == 401 && error.config && !error.config._isRetry && except) {
         originalRequest._isRetry = true;
-        const response = await axios.post<AuthResponse>(`http://localhost:3001/auth/refresh`, {}, {withCredentials: true})
+        const response = await axios.post<AuthResponse>(`${process.env.BACKEND_URL}/auth/refresh`, {}, {withCredentials: true})
         localStorage.setItem('token', response.data.access_token);
         return api.request(originalRequest);
     }
